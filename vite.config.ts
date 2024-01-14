649a8ec5b2383bb72a47af65163b69d1a9192b35
Frederik Adamsen <fread@pop-os.localdomain>
2024-01-14 20:39:21 +0100

    First commit.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite';
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    VueRouter({
+      routesFolder: './routes'
+    }),
+    Vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
